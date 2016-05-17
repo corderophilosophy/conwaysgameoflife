50e485ebbb376eadf7626bb9a1527085b4644eff
Rich Cordero <rich@corderophilosophy.com>
2016-05-17 19:24:43 -0400

    Initial commit. Setup project.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// Gulp dependencies go here
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function() {
+  // Gulp tasks go here
+  // Run eslint
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+
+  // browser source
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+});
