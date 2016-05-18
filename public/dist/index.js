4c78e971a195fa46f7e2b0435ca6a11636490b47
Rich Cordero <rich@corderophilosophy.com>
2016-05-18 11:22:28 -0400

    Populate grid. Count living neighbors.

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,48 +1,90 @@
 "use strict";
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var container = document.querySelector('.container');
 
+// Helper fn to pad index
+function digitPad(number) {
+  var digit = String(number);
+  if (digit.length < 2) {
+    digit = "0" + digit;
+  }
+  return digit;
+}
+
+// Build the grid
 function buildGrid(rows, cols) {
   var grid = "";
   for (var i = 0; i < rows; i += 1) {
+    grid += "<div class=row-" + i + ">";
     for (var j = 0; j < cols; j += 1) {
       grid += "<span class='critter'";
       grid += "data-critter=" + i + j + " ";
       grid += "data-alive='false' ";
+      grid += "data-livingneighbors='0' ";
       grid += "data-next=' '></span>";
     }
-    grid += "<br/>";
+    grid += "</div>";
   }
   return grid;
 }
 
 container.innerHTML = buildGrid(10, 10);
 
+// Get all critters and an array to work with them
 var World = document.querySelectorAll('.critter');
-var world21 = document.querySelector("[data-critter='21']");
-var world212 = World[21];
+var worldArray = [].concat(_toConsumableArray(World));
 
-console.log("world212.dataset.alive is: " + world21.dataset.alive);
-console.log("world212 is: " + world212);
-console.log("before world21.dataset.alive is: " + world21.dataset.alive);
+console.log(World.length);
 
-world21.dataset.alive = false;
-
-console.log("after world21.dataset.critter is: " + world21.dataset.alive);
-
-World.forEach(function (current) {
-  var i = Math.round(Math.random()) + 1;
-  if (i > 1) {
-    current.dataset.alive = 'true';
+// Randomly populate world
+worldArray.forEach(function (current, index) {
+  var i = Math.round(Math.random() * 10);
+  if (i < 3) {
+    World[index].dataset.alive = 'true';
   }
 });
 
-document.querySelectorAll("[data-alive='true']").style.background = "red";
+// Give every item a class - living or dead
+worldArray.forEach(function (current, index) {
+  if (World[index].dataset.alive === 'true') {
+    var idx = digitPad(index);
+    document.querySelector("[data-critter=\"" + idx + "\"]").classList.add('living');
+  } else {
+    var _idx = digitPad(index);
+    document.querySelector("[data-critter=\"" + _idx + "\"]").classList.add('dead');
+  }
+});
 
-/* TODO:
-// 1. Grab the container.
-// 2. Run buildGrid(x,y)
-3. Render critter
-4. Profit.
+// Count up each item's live neighbors
+var liveNeighbors = worldArray.forEach(function (current, index) {
+  var neighbors = 0;
+  if (!!World[index + 1] && World[index + 1].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index - 1] && World[index - 1].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index + 9] && World[index + 9].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index - 9] && World[index - 9].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index + 10] && World[index + 10].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index - 10] && World[index - 10].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index + 11] && World[index + 11].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if (!!World[index - 11] && World[index - 11].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  World[index].dataset.livingneighbors = "" + neighbors;
+});
 
-*/
+liveNeighbors;
--- public/es6/index.js
+++ public/es6/index.js
@@ -1,46 +1,88 @@
 const container = document.querySelector('.container');
 
+// Helper fn to pad index
+function digitPad(number) {
+  let digit = String(number);
+  if (digit.length < 2) {
+    digit = "0" + digit;
+  }
+  return digit;
+}
+
+// Build the grid
 function buildGrid(rows, cols) {
   let grid = "";
   for (let i = 0; i < rows; i += 1) {
+    grid += `<div class=row-${i}>`;
     for (let j = 0; j < cols; j += 1) {
       grid += "<span class='critter'";
       grid += `data-critter=${i}${j} `;
       grid += "data-alive='false' ";
+      grid += "data-livingneighbors='0' ";
       grid += "data-next=' '></span>";
     }
-    grid += "<br/>";
+    grid += "</div>";
   }
   return grid;
 }
 
 container.innerHTML = buildGrid(10, 10);
 
+// Get all critters and an array to work with them
 const World = document.querySelectorAll('.critter');
-const world21 = document.querySelector("[data-critter='21']");
-const world212 = World[21];
+var worldArray = [...World];
 
-console.log("world212.dataset.alive is: " + world21.dataset.alive);
-console.log("world212 is: " + world212);
-console.log("before world21.dataset.alive is: " + world21.dataset.alive);
+console.log(World.length);
 
-world21.dataset.alive = false;
-
-console.log("after world21.dataset.critter is: " + world21.dataset.alive);
-
-World.forEach(function(current) {
-  let i = Math.round(Math.random()) + 1;
-  if (i > 1) {
-    current.dataset.alive = 'true';
+// Randomly populate world
+worldArray.forEach(function(current, index) {
+  let i = Math.round(Math.random() * 10);
+  if (i < 3) {
+    World[index].dataset.alive = 'true';
   }
 });
 
-document.querySelectorAll("[data-alive='true']").style.background = "red";
+// Give every item a class - living or dead
+worldArray.forEach(function(current, index) {
+  if (World[index].dataset.alive === 'true') {
+    let idx = digitPad(index);
+    document.querySelector(`[data-critter="${idx}"]`)
+      .classList.add('living');
+  } else {
+    let idx = digitPad(index);
+    document.querySelector(`[data-critter="${idx}"]`)
+      .classList.add('dead');
+  }
+});
 
-/* TODO:
-// 1. Grab the container.
-// 2. Run buildGrid(x,y)
-3. Render critter
-4. Profit.
+// Count up each item's live neighbors
+const liveNeighbors = worldArray.forEach(function(current, index) {
+  let neighbors = 0;
+  if ((!!World[index + 1]) && World[index + 1].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index - 1]) && World[index - 1].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index + 9]) && World[index + 9].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index - 9]) && World[index - 9].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index + 10]) && World[index + 10].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index - 10]) && World[index - 10].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index + 11]) && World[index + 11].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  if ((!!World[index - 11]) && World[index - 11].dataset.alive === 'true') {
+    neighbors += 1;
+  }
+  World[index].dataset.livingneighbors = `${neighbors}`;
+});
 
-*/
+liveNeighbors;
