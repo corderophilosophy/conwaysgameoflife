f53de7be5fc36abf21ea6d0b1799a9010f997e76
Rich Cordero <rich@corderophilosophy.com>
2016-05-18 14:52:45 -0400

    Finish code.

--- public/dist/index.js
+++ public/dist/index.js
@@ -13,21 +13,29 @@ function digitPad(number) {
   return digit;
 }
 
+// Helper fn to clear data-next
+function clearNext() {
+  worldArray.forEach(function (element, index) {
+    World[index].dataset.next = "";
+  });
+}
+
 // Build the grid
 function buildGrid(rows, cols) {
-  var grid = "";
+  var grid = "<div class='grid'>";
   for (var i = 0; i < rows; i += 1) {
-    grid += "<div class=row-" + i + ">";
+    grid += "<div class='row row-" + i + "'>";
     for (var j = 0; j < cols; j += 1) {
       grid += "<span class='critter'";
       grid += "data-critter=" + i + j + " ";
       grid += "data-alive='false' ";
-      grid += "data-livingneighbors='0' ";
+      grid += "data-livingneighbors=0 ";
       grid += "data-next=''>";
       grid += "</span>";
     }
     grid += "</div>";
   }
+  grid += "</div>";
   return grid;
 }
 
@@ -40,80 +48,125 @@ var worldArray = [].concat(_toConsumableArray(World));
 console.log(World.length);
 
 // Randomly populate world
-worldArray.forEach(function (current, index) {
-  var i = Math.round(Math.random() * 10);
-  if (i < 3) {
-    World[index].dataset.alive = 'true';
-  }
-});
+function populateWorld() {
+  worldArray.forEach(function (current, index) {
+    var i = Math.round(Math.random() * 10);
+    if (i < 3) {
+      World[index].dataset.alive = 'true';
+    }
+  });
+}
+
+// // Populate World
+// function populateWorld() {
+//   World[43].dataset.alive = 'true';
+//   World[44].dataset.alive = 'true';
+//   World[45].dataset.alive = 'true';
+//   World[32].dataset.alive = 'true';
+//   World[33].dataset.alive = 'true';
+//   World[34].dataset.alive = 'true';
+// }
 
 // Give every item a class - living or dead
-worldArray.forEach(function (current, index) {
-  if (World[index].dataset.alive === 'true') {
-    var idx = digitPad(index);
-    document.querySelector("[data-critter=\"" + idx + "\"]").classList.add('living');
-  } else {
-    var _idx = digitPad(index);
-    document.querySelector("[data-critter=\"" + _idx + "\"]").classList.add('dead');
-  }
-});
-
+function classifyLifeDeath() {
+  worldArray.forEach(function (current, index) {
+    if (World[index].dataset.alive === 'true') {
+      var idx = digitPad(index);
+      document.querySelector("[data-critter=\"" + idx + "\"]").classList.add('living');
+    } else {
+      var _idx = digitPad(index);
+      document.querySelector("[data-critter=\"" + _idx + "\"]").classList.add('dead');
+    }
+  });
+}
 // Count up each item's live neighbors
-var liveNeighbors = worldArray.forEach(function (current, index) {
-  var neighbors = 0;
-  if (!!World[index + 1] && World[index + 1].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index - 1] && World[index - 1].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index + 9] && World[index + 9].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index - 9] && World[index - 9].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index + 10] && World[index + 10].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index - 10] && World[index - 10].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index + 11] && World[index + 11].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if (!!World[index - 11] && World[index - 11].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  World[index].dataset.livingneighbors = "" + neighbors;
-});
-
-liveNeighbors;
+function liveNeighbors() {
+  worldArray.forEach(function (current, index) {
+    var neighbors = 0;
+    World[index].dataset.livingneighbors = '0';
+    if (!!World[index + 1] && World[index + 1].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index - 1] && World[index - 1].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index + 9] && World[index + 9].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index - 9] && World[index - 9].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index + 10] && World[index + 10].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index - 10] && World[index - 10].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index + 11] && World[index + 11].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if (!!World[index - 11] && World[index - 11].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    World[index].dataset.livingneighbors = "" + neighbors;
+  });
+}
 
 // Stage life and death for next cycle
 function stageLifeDeath() {
   worldArray.forEach(function (current, index) {
     // Underpopulation deaths
-    if (World[index].dataset.alive === 'true' && World[index].dataset.livingneighbors < 2) {
+    if (World[index].dataset.alive === 'true' && Number(World[index].dataset.livingneighbors) < 2) {
       World[index].dataset.next = "dead";
     }
     // Persistence
-    if (World[index].dataset.alive === 'true' && (World[index].dataset.livingneighbors == 2 || World[index].dataset.livingneighbors == 3)) {
+    if (World[index].dataset.alive === 'true' && (Number(World[index].dataset.livingneighbors) === 2 || Number(World[index].dataset.livingneighbors) === 3)) {
       World[index].dataset.next = "alive";
     }
     // Overpopulation deaths
-    if (World[index].dataset.alive === 'true' && World[index].dataset.livingneighbors > 3) {
+    if (World[index].dataset.alive === 'true' && Number(World[index].dataset.livingneighbors) > 3) {
       World[index].dataset.next = "dead";
     }
     // Rebirth by reproduction
-    if (World[index].dataset.alive === 'false' && World[index].dataset.livingneighbors == 3) {
+    if (World[index].dataset.alive === 'false' && Number(World[index].dataset.livingneighbors) == '3') {
       World[index].dataset.next = "alive";
     }
     // Persistent death
-    else {
-        World[index].dataset.next = "dead";
-      }
+    // else {
+    //   World[index].dataset.next = "dead";
+    // }
   });
 }
 
+function iterate() {
+  console.log('clicked');
+  worldArray.forEach(function (current, index) {
+    if (World[index].dataset.next === "alive") {
+      var idx = digitPad(index);
+      document.querySelector("[data-critter=\"" + idx + "\"]").classList.add('living');
+      document.querySelector("[data-critter=\"" + idx + "\"]").classList.remove('dead');
+      World[index].dataset.alive = 'true';
+    }
+    if (World[index].dataset.next === "dead") {
+      var _idx2 = digitPad(index);
+      document.querySelector("[data-critter=\"" + _idx2 + "\"]").classList.add('dead');
+      document.querySelector("[data-critter=\"" + _idx2 + "\"]").classList.remove('living');
+      World[index].dataset.alive = 'false';
+    }
+  });
+  clearNext();
+  liveNeighbors();
+  stageLifeDeath();
+}
+
+var btn = document.querySelector('[data-action="iterator"]');
+
+btn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  iterate();
+});
+
+populateWorld();
+classifyLifeDeath();
+liveNeighbors();
 stageLifeDeath();
--- public/es6/index.js
+++ public/es6/index.js
@@ -9,21 +9,29 @@ function digitPad(number) {
   return digit;
 }
 
+// Helper fn to clear data-next
+function clearNext() {
+  worldArray.forEach(function(element, index) {
+    World[index].dataset.next = "";
+  });
+}
+
 // Build the grid
 function buildGrid(rows, cols) {
-  let grid = "";
+  let grid = "<div class='grid'>";
   for (let i = 0; i < rows; i += 1) {
-    grid += `<div class=row-${i}>`;
+    grid += `<div class='row row-${i}'>`;
     for (let j = 0; j < cols; j += 1) {
       grid += "<span class='critter'";
       grid += `data-critter=${i}${j} `;
       grid += "data-alive='false' ";
-      grid += "data-livingneighbors='0' ";
+      grid += "data-livingneighbors=0 ";
       grid += "data-next=''>";
       grid += "</span>";
     }
     grid += "</div>";
   }
+  grid += "</div>";
   return grid;
 }
 
@@ -36,82 +44,123 @@ var worldArray = [...World];
 console.log(World.length);
 
 // Randomly populate world
-worldArray.forEach(function(current, index) {
-  let i = Math.round(Math.random() * 10);
-  if (i < 3) {
-    World[index].dataset.alive = 'true';
-  }
-});
+function populateWorld() {
+  worldArray.forEach(function(current, index) {
+    let i = Math.round(Math.random() * 10);
+    if (i < 3) {
+      World[index].dataset.alive = 'true';
+    }
+  });
+}
+
 
 // Give every item a class - living or dead
-worldArray.forEach(function(current, index) {
-  if (World[index].dataset.alive === 'true') {
-    let idx = digitPad(index);
-    document.querySelector(`[data-critter="${idx}"]`)
-      .classList.add('living');
-  } else {
-    let idx = digitPad(index);
-    document.querySelector(`[data-critter="${idx}"]`)
-      .classList.add('dead');
-  }
-});
-
+function classifyLifeDeath() {
+  worldArray.forEach(function(current, index) {
+    if (World[index].dataset.alive === 'true') {
+      let idx = digitPad(index);
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.add('living');
+    } else {
+      let idx = digitPad(index);
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.add('dead');
+    }
+  });
+}
 // Count up each item's live neighbors
-const liveNeighbors = worldArray.forEach(function(current, index) {
-  let neighbors = 0;
-  if ((!!World[index + 1]) && World[index + 1].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index - 1]) && World[index - 1].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index + 9]) && World[index + 9].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index - 9]) && World[index - 9].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index + 10]) && World[index + 10].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index - 10]) && World[index - 10].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index + 11]) && World[index + 11].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  if ((!!World[index - 11]) && World[index - 11].dataset.alive === 'true') {
-    neighbors += 1;
-  }
-  World[index].dataset.livingneighbors = `${neighbors}`;
-});
-
-liveNeighbors;
+function liveNeighbors() {
+  worldArray.forEach(function(current, index) {
+    let neighbors = 0;
+    World[index].dataset.livingneighbors = '0';
+    if ((!!World[index + 1]) && World[index + 1].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index - 1]) && World[index - 1].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index + 9]) && World[index + 9].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index - 9]) && World[index - 9].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index + 10]) && World[index + 10].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index - 10]) && World[index - 10].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index + 11]) && World[index + 11].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    if ((!!World[index - 11]) && World[index - 11].dataset.alive === 'true') {
+      neighbors += 1;
+    }
+    World[index].dataset.livingneighbors = `${neighbors}`;
+  });
+}
 
 // Stage life and death for next cycle
 function stageLifeDeath() {
   worldArray.forEach(function(current, index) {
     // Underpopulation deaths
-    if (World[index].dataset.alive === 'true' && World[index].dataset.livingneighbors < 2) {
+    if (World[index].dataset.alive === 'true' && Number(World[index].dataset.livingneighbors) < 2) {
       World[index].dataset.next = "dead";
     }
     // Persistence
-    if (World[index].dataset.alive === 'true' && (World[index].dataset.livingneighbors == 2 || World[index].dataset.livingneighbors == 3)) {
+    if (World[index].dataset.alive === 'true' && (Number(World[index].dataset.livingneighbors) === 2 || Number(World[index].dataset.livingneighbors) === 3)) {
       World[index].dataset.next = "alive";
     }
     // Overpopulation deaths
-    if (World[index].dataset.alive === 'true' && World[index].dataset.livingneighbors > 3) {
+    if (World[index].dataset.alive === 'true' && Number(World[index].dataset.livingneighbors) > 3) {
       World[index].dataset.next = "dead";
     }
     // Rebirth by reproduction
-    if (World[index].dataset.alive === 'false' && World[index].dataset.livingneighbors == 3) {
+    if (World[index].dataset.alive === 'false' && Number(World[index].dataset.livingneighbors) == '3') {
       World[index].dataset.next = "alive";
     }
     // Persistent death
-    else {
-      World[index].dataset.next = "dead";
-    }
+    // else {
+    //   World[index].dataset.next = "dead";
+    // }
   });
 }
 
+function iterate() {
+  console.log('clicked');
+  worldArray.forEach(function(current, index) {
+    if (World[index].dataset.next === "alive") {
+      let idx = digitPad(index);
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.add('living');
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.remove('dead');
+      World[index].dataset.alive = 'true';
+    }
+    if (World[index].dataset.next === "dead") {
+      let idx = digitPad(index);
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.add('dead');
+      document.querySelector(`[data-critter="${idx}"]`)
+        .classList.remove('living');
+      World[index].dataset.alive = 'false';
+    }
+  });
+  clearNext();
+  liveNeighbors();
+  stageLifeDeath();
+}
+
+const btn = document.querySelector('[data-action="iterator"]');
+
+btn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  iterate();
+});
+
+
+populateWorld();
+classifyLifeDeath();
+liveNeighbors();
 stageLifeDeath();
